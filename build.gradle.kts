72fc80d2b0783235933de0d9185ba07994ca6c44
ByrtZ <taylor.byrne417@gmail.com>
2024-09-04 23:59:15 +0100

    The start of something epic.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
-
     implementation("org.incendo:cloud-core:2.0.0-rc.2")
     implementation("org.incendo:cloud-paper:2.0.0-beta.9")
     implementation("org.incendo:cloud-annotations:2.0.0-rc.2")
--- src/main/kotlin/dev/byrt/burb/Main.kt
+++ src/main/kotlin/dev/byrt/burb/Main.kt
@@ -4,12 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
-        // Plugin startup logic
         logger.info("Starting Burb plugin...")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
         logger.info("Cleaning up Burb plugin...")
     }
 }
