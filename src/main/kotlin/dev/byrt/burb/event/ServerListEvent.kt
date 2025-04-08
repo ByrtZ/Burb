e3cfb637277d4d6421889710a065b387ceca27ba
ByrtZ <taylor.byrne417@gmail.com>
2025-04-08 17:30:47 +0100

    Update to 1.21.4

--- src/main/kotlin/dev/byrt/burb/event/ServerListEvent.kt
+++ src/main/kotlin/dev/byrt/burb/event/ServerListEvent.kt
@@ -4,14 +4,16 @@ import com.destroystokyo.paper.event.server.PaperServerListPingEvent
 
 import dev.byrt.burb.chat.ChatUtility
 import dev.byrt.burb.library.Translation
+import org.bukkit.Bukkit
 
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
+@Suppress("unused")
 class ServerListEvent: Listener {
     @EventHandler
     private fun onServerPing(e: PaperServerListPingEvent) {
-        e.version = "Byrtrium v1.21.1"
+        e.version = "Byrtrium v${Bukkit.getMinecraftVersion()}"
         e.motd(ChatUtility.formatMessage("${Translation.TabList.SERVER_LIST_PADDING}${Translation.TabList.SERVER_LIST_TITLE}${Translation.TabList.SERVER_LIST_PADDING}<newline>${Translation.TabList.SERVER_LIST_VERSION}${Translation.TabList.SERVER_LIST_GAME}${Translation.TabList.SERVER_LIST_EXTRA}", false))
     }
 }
